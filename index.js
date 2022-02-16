8771c7a1f799d00c9db24327113bdc9bfe793c9f
Neo <neo_official@outlook.com>
2022-02-17 01:16:34 +0330

    App Updated: (v-1.5.2)
    Fixed Bugs

--- index.js
+++ index.js
@@ -153,9 +153,30 @@ function imageDataToBase64 (data = []) {
   data = window.btoa(data);
   return data ? 'data:image/png;base64,' + data : DEFAULT_IMAGE;
 }
+function dataURItoBlob(dataURI) {
+  // convert base64 to raw binary data held in a string
+  // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+  var byteString = atob(dataURI.split(',')[1]);
+  // separate out the mime component
+  var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
 
+  // write the bytes of the string to an ArrayBuffer
+  var ab = new ArrayBuffer(byteString.length);
+
+  // create a view into the buffer
+  var ia = new Uint8Array(ab);
+
+  // set the bytes of the buffer to the correct values
+  for (var i = 0; i < byteString.length; i++) {
+	ia[i] = byteString.charCodeAt(i);
+  }
+
+  // write the ArrayBuffer to a blob, and you're done
+  return new Blob([ab], {type: mimeString});
+
+}
 function imageDataToBlob (data) {
-  return data == null ? DEFAULT_BLOB : new Blob([...data], {type: 'image/png'});
+  return data == null ? DEFAULT_BLOB : dataURItoBlob(imageDataToBase64(data));
 }
 
 function blobToUrl (blob) {
